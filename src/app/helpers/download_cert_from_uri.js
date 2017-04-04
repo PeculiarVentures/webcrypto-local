302d231f2f377d3861f249892f3408ebeac2ddd4
donskov <donskovdmitriyv@gmail.com>
2017-04-05 00:31:17 +0300

    Fixes #37

--- src/app/helpers/download_cert_from_uri.js
+++ src/app/helpers/download_cert_from_uri.js
@@ -1,7 +1,7 @@
 import downloadURI from './download_uri';
 
 export default function downloadCertFromURI(name, value) {
-  const fileName = `${name}.cer`;
-  const key = `data:application/pkix-cert;charset=utf-8,${encodeURIComponent(value)}`;
+  const fileName = `${name}.csr`;
+  const key = `data:application/pkcs10;charset=utf-8,${encodeURIComponent(value)}`;
   downloadURI(key, fileName);
 }
