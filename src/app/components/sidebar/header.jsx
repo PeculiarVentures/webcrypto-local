0c4da89edcffb8929be6194849c2ea18ff5196c5
donskov <donskovdmitriyv@gmail.com>
2017-04-07 17:43:10 +0300

    Fixes #51

--- src/app/components/sidebar/header.jsx
+++ src/app/components/sidebar/header.jsx
@@ -1,55 +1,9 @@
 import React, { PropTypes, Component } from 'react';
-import styled from 'styled-components';
-import { Button, SelectField, SelectItem, SelectNative } from '../basic';
-import { ImportIcon, CreateIcon } from '../svg';
+import { SelectField, SelectItem, SelectNative } from '../basic';
 import { RoutingActions, ModalActions } from '../../actions/ui';
 import { ProviderActions } from '../../actions/state';
-// import { AccessedMimes, AccessedExtentions } from '../../constants/acessed_types';
 import enLang from '../../langs/en.json';
-
-const CreateIconStyled = styled(CreateIcon)`
-  width: 14px;
-  margin-top: -3px;
-`;
-
-const ImportIconStyled = styled(ImportIcon)`
-  width: 17px;
-  margin-top: -2px;
-`;
-
-const BtnsContainer = styled.div`
-  background: ${props => props.theme.sidebar.backgroundHeader};
-  padding: 24px 30px;
-  @media ${props => props.theme.media.mobile} {
-    padding: 12px;
-  }
-`;
-
-const SelectContainer = styled.div`
-  padding: 24px 25px 8px;
-  @media ${props => props.theme.media.mobile} {
-    padding: 13px 12px 5px;
-  }
-`;
-
-const ButtonStyled = styled(Button)`
-  margin-left: 6px;
-  width: calc(50% - 3px);
-  padding: 0 10px !important;
-  &:first-child {
-    margin-left: 0;
-  }
-  position: relative;
-`;
-
-const SidebarHeaderStyled = styled.div`
-  height: 164px;
-  z-index: 1;
-  position: relative;
-  @media ${props => props.theme.media.mobile} {
-    height: 122px;
-  }
-`;
+import * as HeaderStyled from './styled/header.styled';
 
 export default class SidebarHeader extends Component {
 
@@ -78,48 +32,7 @@ export default class SidebarHeader extends Component {
   onClickImportHandler = () => {
     const { dispatch } = this.context;
     dispatch(ModalActions.openModal('import_certificate'));
-    // event.preventDefault();
-    // console.log(event.target.files);
-    // event.preventDefault();
-    // const filesArray = event.target.files;
-    // const promises = [];
-    //
-    // for (let i = 0; i < filesArray.length; i += 1) {
-    //   const reader = new FileReader();
-    //   const file = filesArray[i];
-    //
-    //   if (AccessedMimes.indexOf(file.type) === -1) {
-    //     continue;
-    //   }
-    //
-    //   promises.push(new Promise((resolve) => {
-    //     reader.onload = (e) => {
-    //       const nameParts = file.name.split('.');
-    //
-    //       resolve({
-    //         file: e.target.result,
-    //         name: nameParts[0],
-    //         extension: nameParts[nameParts.length - 1],
-    //         type: file.type,
-    //       });
-    //     };
-    //
-    //     reader.readAsArrayBuffer(file);
-    //   }));
-    // }
-    //
-    // return Promise.all(promises).then((files) => {
-    //   console.log('files', files);
-    // });
   };
-
-  // getAcceptString() {
-  //   const { deviceType } = this.context;
-  //   if (deviceType === 'desktop') {
-  //     return AccessedMimes.join(',');
-  //   }
-  //   return AccessedExtentions.map(e => `.${e}`).join(', ');
-  // }
 
   onSelectHandler = (data) => {
     const { dispatch } = this.context;
@@ -133,33 +46,32 @@ export default class SidebarHeader extends Component {
   render() {
     const { dataLoaded, providers } = this.props;
     const { deviceType } = this.context;
-    // const acceptString = this.getAcceptString();
     const selectedProvider = providers.filter(obj => obj.selected);
     const currentProvider = selectedProvider.length
       ? selectedProvider[0]
       : false;
 
     return (
-      <SidebarHeaderStyled>
-        <BtnsContainer>
-          <ButtonStyled
+      <HeaderStyled.SidebarHeader>
+        <HeaderStyled.BtnsContainer>
+          <HeaderStyled.Btn
             disabled={!dataLoaded}
             primary
             onClick={this.onClickImportHandler}
           >
-            <ImportIconStyled />
+            <HeaderStyled.ImportIc />
             { enLang['Sidebar.Header.Btn.Import'] }
-          </ButtonStyled>
-          <ButtonStyled
+          </HeaderStyled.Btn>
+          <HeaderStyled.Btn
             disabled={!dataLoaded}
             primary
             onClick={this.onClickCreateHandler}
           >
-            <CreateIconStyled />
+            <HeaderStyled.CreateIc />
             { enLang['Sidebar.Header.Btn.Create'] }
-          </ButtonStyled>
-        </BtnsContainer>
-        <SelectContainer>
+          </HeaderStyled.Btn>
+        </HeaderStyled.BtnsContainer>
+        <HeaderStyled.SelectContainer>
           {
             deviceType === 'phone'
               ? <SelectNative
@@ -193,8 +105,8 @@ export default class SidebarHeader extends Component {
                 }
               </SelectField>
           }
-        </SelectContainer>
-      </SidebarHeaderStyled>
+        </HeaderStyled.SelectContainer>
+      </HeaderStyled.SidebarHeader>
     );
   }
 }
--- src/app/containers/root.jsx
+++ src/app/containers/root.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import Info from '../components/info/index';
-import { Sidebar } from '../components/sidebar';
+import Sidebar from '../components/sidebar/index';
 import Overlay from './overlay';
 import { CertificateActions } from '../actions/state';
 import { RoutingActions } from '../actions/ui';
