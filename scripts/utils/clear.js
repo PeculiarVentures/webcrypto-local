368a10ad547ae5f420e66c491f3b11132476269c
microshine <microshine@mail.ru>
2017-04-11 22:48:39 +0300

    Update clear script for Windows

--- scripts/utils/clear.js
+++ scripts/utils/clear.js
@@ -1,8 +1,25 @@
 import sh from 'sh';
+import os from 'os';
+import chalk from 'chalk';
 import { OUTPUT_PATH } from '../config';
+const exec = require('child_process').exec;
 
-const print = require('./print')('clear');
+let clear;
+if (os.type() === "Windows_NT") {
+  clear = () => {
+    return new Promise((resolve, reject) => {
+      exec(`RD /S /Q ${OUTPUT_PATH}`, (error, stdout, stderr) => {
+        resolve();
+      })
+    })
+      .then(() => {
+        console.log(chalk.blue('> Build cleared successfully.'));
+      });
+  }
+} else {
+  clear = () => sh(`rm -rf ${OUTPUT_PATH}`).then(() => {
+    console.log(chalk.blue('> Build cleared successfully.'));
+  });
+}
 
-export default () => sh(`rm -rf ${OUTPUT_PATH}`).then(() => {
-  print.log('Build cleared succesfully.');
-});
+export default clear;
