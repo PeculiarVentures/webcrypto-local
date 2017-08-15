3d3c9f7cbdb1894b096a65969ec4a79b15405639
donskov <donskovdmitriyv@gmail.com>
2017-08-15 23:15:03 +0300

    fix error for IE11

--- src/app/helpers/compose.js
+++ src/app/helpers/compose.js
@@ -2,7 +2,7 @@
 export default function () {
   return (state, payload) => {
     let _s = state;
-    for (const f of arguments) {
+    for (const [key, f] of Object.entries(arguments)) {
       _s = f(_s, payload);
     }
     return _s;
