e3cc7788b1ffd769937a3a056099c7795bab8ec5
microshine <microshine@mail.ru>
2017-12-07 23:26:58 +0300

    Update types

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,45 @@ import * as https from "https";
 import { ObjectProto } from "tsprotobuf";
 import * as WebSocket from "websocket";
 
+export enum WebCryptoLocalErrorEnum {
+    UNKNOWN = 0,
+    METHOD_NOT_IMPLEMENTED = 1,
+    CASE_ERROR = 2,
+    RATCHET_COMMON = 100,
+    RATCHET_KEY_NOT_APPROVED = 101,
+    ACTION_COMMON = 200,
+    ACTION_NOT_IMPLEMENTED = 201,
+    ACTION_NOT_SUPPORTED = 202,
+    CARD_CONFIG_COMMON = 300,
+    MEMORY_STORAGE_COMMON = 350,
+    MEMORY_STORAGE_OUT_OF_INDEX = 351,
+    PROVIDER_COMMON = 400,
+    PROVIDER_INIT = 401,
+    PROVIDER_CRYPTO_NOT_FOUND = 402,
+    PROVIDER_NOT_FOUND = 403,
+    PROVIDER_WRONG_LIBRARY = 404,
+    TOKEN_COMMON = 500,
+    TOKEN_REMOVE_TOKEN_READING = 501,
+    TOKEN_REMOVE_NO_SLOTS_FOUND = 502,
+    SERVER_COMMON = 600,
+    SERVER_WRONG_MESSAGE = 601,
+    SERVER_NOT_LOGGED_IN = 602,
+}
+
+export class WebCryptoLocalError extends Error {
+
+    public static CODE: typeof WebCryptoLocalErrorEnum;
+
+    public code: number;
+    public type: string;
+    public stack: string;
+
+    constructor(code: number, message?: string);
+    constructor(message: string);
+
+}
+
+
 export class BaseProto extends ObjectProto {
     public static INDEX: number;
     public version: number;
@@ -259,7 +298,6 @@ export class LocalServer extends EventEmitter {
     public on(event: "info", cb: (message: string) => void): this;
     public on(event: "listening", cb: Function): this;
     public on(event: "token_new", cb: (card: PCSCCard) => void): this;
-    public on(event: "token_error", cb: (message: string) => void): this;
     public on(event: "error", cb: Function): this;
     public on(event: "close", cb: Function): this;
     public on(event: "notify", cb: Function): this;
