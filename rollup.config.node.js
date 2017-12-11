55cc6a105a43ff3fa76c9a664da2b015a208306a
microshine <microshine@mail.ru>
2017-12-11 13:10:54 +0300

    rollup for server

---
+++ rollup.config.node.js
@@ -0,0 +1,42 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript";
+
+let pkg = require("./package.json");
+
+let banner = []
+
+export default {
+    input: "src/local/index.ts",
+    plugins: [
+        typescript({ typescript: require("typescript"), target: "es5", removeComments: true }),
+        nodeResolve({ jsnext: true, main: true }),
+    ],
+    external: [
+        "2key-ratchet",
+        "asn1js",
+        "event-emitter",
+        "graphene-pk11",
+        "idb",
+        "webcrypto-core",
+        "node-webcrypto-ossl",
+        "node-webcrypto-p11",
+        "node-webcrypto-p11/built/subtle",
+        "node-webcrypto-p11/built/cert_storage",
+        "pcsclite",
+        "pkcs11js",
+        "pkijs",
+        "pvtsutils",
+        "pvutils",
+        "request",
+        "tslib",
+        "tsprotobuf",
+        "websocket"
+    ],
+    banner: banner.join("\n"),
+    output: [
+        {
+            file: "dist/webcrypto-local.js",
+            format: "cjs",
+        }
+    ]
+};
