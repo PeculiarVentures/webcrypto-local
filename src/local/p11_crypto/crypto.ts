2dbf7d06bd2a57285dc2ce6f2c2740bb4df0909d
microshine <microshine@mail.ru>
2018-08-07 15:32:16 +0300

    Fix TS v3 error

--- src/local/p11_crypto/crypto.ts
+++ src/local/p11_crypto/crypto.ts
@@ -38,4 +38,7 @@ export class Pkcs11Crypto extends WebCrypto {
         return ++this.osslID;
     }
 
+    public getRandomValues<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | null>(array: T): T {
+        return super.getRandomValues(array) as any;
+    }
 }
