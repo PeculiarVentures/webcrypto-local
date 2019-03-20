014f736743f36d266a6bcd1e46247ff77b58eae6
microshine <microshine@mail.ru>
2019-03-21 01:46:35 +0300

    Build browser version for client

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -1,3 +1,7 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import builtins from "rollup-plugin-node-builtins";
+import commonjs from "rollup-plugin-commonjs";
 import { dts, ts } from "rollup-plugin-dts";
 
 const pkg = require("./package.json");
@@ -7,42 +11,114 @@ const input = "src/index.ts";
 const external = Object.keys(pkg.dependencies)
   .concat(["events"]);
 
-export default [
+// main
+const main = {
+  input,
+  plugins: [
+    ts({
+      compilerOptions: {
+        removeComments: true,
+      },
+    }),
+  ],
+  external,
+  output: [
+    {
+      banner,
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      banner,
+      file: pkg.module,
+      format: "es",
+    }
+  ]
+};
+
+// types
+const types = {
+  input,
+  plugins: [
+    dts(),
+  ],
+  external,
+  output: [
+    {
+      banner,
+      file: pkg.types,
+      format: "es"
+    }
+  ]
+};
+
+const browserExternals = {
+  "protobufjs": "protobufjs",
+  "ws": "WebSocket",
+  "node-fetch": "fetch",
+};
+const browser = [
   {
     input,
     plugins: [
+      resolve(),
+      commonjs(),
+      builtins({
+        events: true,
+      }),
       ts({
         compilerOptions: {
           removeComments: true,
         },
       }),
     ],
-    external,
+    external: Object.keys(browserExternals),
     output: [
       {
-        banner,
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        banner,
-        file: pkg.module,
+        file: pkg.browser,
         format: "es",
+        globals: browserExternals,
       }
     ]
   },
   {
-    input,
+    input: pkg.browser,
     plugins: [
-      dts(),
+      babel({
+        babelrc: false,
+        runtimeHelpers: true,
+        presets: [
+          [
+            "@babel/env",
+            {
+              targets: {
+                // ie: "11",
+                chrome: "60",
+              },
+              useBuiltIns: "entry"
+            }
+          ]
+        ],
+        plugins: [
+          "@babel/proposal-class-properties",
+          "@babel/proposal-object-rest-spread",
+        ],
+      }),
     ],
-    external,
+    external: Object.keys(browserExternals),
     output: [
       {
-        banner,
-        file: pkg.types,
-        format: "es"
-      }
-    ]
-  },
+        file: pkg.browser,
+        format: "iife",
+        name: "WebcryptoSocket",
+        globals: browserExternals,
+      },
+    ],
+  }
+]
+
+export default [
+  // main,
+  types,
+  // ...browser,
 ];
