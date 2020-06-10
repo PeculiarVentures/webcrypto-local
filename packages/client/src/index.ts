06a8ed0f08de7ad8ebee3198ecfff2390e161cb6
microshine <microshine@mail.ru>
2020-06-10 14:54:27 +0300

    feat(client): Export Key and Cert storages

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,7 @@
 export { setEngine, getEngine } from "2key-ratchet";
 export { SocketProvider } from "./client";
 export { SocketCrypto } from "./crypto";
+export { CertificateStorage } from "./cert_storage";
+export { KeyStorage } from "./key_storage";
 export * from "./errors";
 export * from "./connection/storages";
