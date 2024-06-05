c67d189c86a566aa48260a83b34ac562824f493e
microshine <microshine@mail.ru>
2024-06-05 13:43:52 +0200

    test: Add console log to display providers in cert_storage test

--- test/client/cert_storage.ts
+++ test/client/cert_storage.ts
@@ -39,8 +39,9 @@ context("WebCrypto Socket", () => {
         await ws.login();
       }
       const info = await ws.info();
+      console.log("providers", info.providers);
       const providers = info.providers.filter((provider) => provider.name === PROVIDER_NAME);
-      assert.strictEqual(providers.length, 1, `Cannot get provider by name ${PROVIDER_NAME}`);
+      assert.strictEqual(providers.length, 1, `Cannot get provider by name '${PROVIDER_NAME}'`);
       crypto = await ws.getCrypto(providers[0].id);
       if (!(await crypto.isLoggedIn())) {
         await crypto.login();
