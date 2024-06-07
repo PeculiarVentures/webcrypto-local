7c71dd0f8ed9180bf228a1d2dee2fed9acf33736
microshine <microshine@mail.ru>
2024-06-07 15:52:06 +0200

    test: getChain with RSA-PSS

--- test/client/cert_storage.ts
+++ test/client/cert_storage.ts
@@ -1,10 +1,11 @@
 import { sha1 } from "@peculiar/asn1-rsa";
 import { OCSPRequest, TBSRequest, Request, CertID, OCSPResponse, OCSPResponseStatus } from "@peculiar/asn1-ocsp";
 import { AsnConvert, OctetString } from "@peculiar/asn1-schema";
+import * as x509 from "@peculiar/x509";
 import * as client from "@webcrypto-local/client";
 import * as assert from "assert";
 import { Convert } from "pvtsutils";
-import { CryptoX509Certificate, CryptoX509CertificateRequest, PemConverter } from "webcrypto-core";
+import { CryptoX509Certificate, CryptoX509CertificateRequest, PemConverter, CryptoCertificate } from "webcrypto-core";
 import { PROVIDER_NAME, SERVER_ADDRESS } from "../config";
 
 context("WebCrypto Socket", () => {
@@ -17,11 +18,11 @@ context("WebCrypto Socket", () => {
     const REQ_RAW = Convert.FromBase64("MIICZzCCAU8CAQAwIjEgMB4GA1UEAwwXbWFjLW1pbmkuYWVnZG9tYWluMS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCyiZeRY2Zi6Xnu+asuL85d29Km+WXIQYOk5oo0IsDH7S2KjR4VyulDfNAx6cH9TQydg4LBzve3adBB9hAxpqJOy1ooaLENlml07uXj758C7La5NioBqTjoohbERnzjSpZqiH+08INPtZDDpPH6pRU3MmmtFSywE0R9KOUGNcLXhFHP3jAbiJo5dQDxWUYpjf4ZUlLwE3k6yjtM7y2UlWFb1pB/207FkV/DY+YUTx0PToylGDHLYWdKUXBgAU/htN1VN5tezRd6wwznIP8OiZ8Ebtctq3loA9PfTYblLirYBvuKMPXYbNjw5Fp5XmMFFX83+zLsND3cn4Susdfu7L0hAgMBAAGgADANBgkqhkiG9w0BAQsFAAOCAQEAHpbQ+KTx98neqGtJ6QpmfRuUoHI0tRuCLzmdnCLblqso0bSLDgRgK+bEHVjnIpPpmfu/ZBbK4Hn73nZZPPqpaKE33Ef8A4fKjRrx+5pVSPjt783xrI8uUnn5fmip5pZL07tP8J/RQN30SIRBnCSKUnIgurA5tDPwayxmpnAoh8D5Jl0vvmNAnbHBKvTBAoJDrHLLMyJlbQh2Ip7GPfvfic8yGvaHYKtLfNR52y77aNEc90g3CymtxdImJRYYCjZuAYbfYk31DeKBNTFl1OM+VOqEAUOv/meRxGsEELHHykbYcOOef610LzbR/D10p9e/zaeXyaXlr/GBHQKPUvenTg==");
 
     let ws: client.SocketProvider;
-    let crypto: client.SocketCrypto;
+    let provider: client.SocketCrypto;
 
     async function addCertificate(certRaw: ArrayBuffer) {
-      const item = await crypto.certStorage.importCert("raw", certRaw, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
-      const certIndex = await crypto.certStorage.setItem(item);
+      const item = await provider.certStorage.importCert("raw", certRaw, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
+      const certIndex = await provider.certStorage.setItem(item);
       return certIndex;
     }
 
@@ -44,118 +45,118 @@ context("WebCrypto Socket", () => {
       const info = await ws.info();
       const providers = info.providers.filter((provider) => provider.name === PROVIDER_NAME);
       assert.strictEqual(providers.length, 1, `Cannot get provider by name '${PROVIDER_NAME}'`);
-      crypto = await ws.getCrypto(providers[0].id);
-      if (!(await crypto.isLoggedIn())) {
-        await crypto.login();
+      provider = await ws.getCrypto(providers[0].id);
+      if (!(await provider.isLoggedIn())) {
+        await provider.login();
       }
     });
 
     it("clear", async () => {
-      await crypto.certStorage.clear();
-      const keys = await crypto.certStorage.keys();
+      await provider.certStorage.clear();
+      const keys = await provider.certStorage.keys();
       assert.equal(keys.length, 0);
     });
 
     context("import/export", () => {
       context("certificate", () => {
         it("raw", async () => {
-          const item = await crypto.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
+          const item = await provider.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
           assert.equal(item.type, "x509");
 
-          const raw = await crypto.certStorage.exportCert("raw", item);
+          const raw = await provider.certStorage.exportCert("raw", item);
           assert.equal(Convert.ToHex(raw), Convert.ToHex(CERT_RAW));
         });
 
         it("pem", async () => {
           const pem = PemConverter.fromBufferSource(CERT_RAW, "certificate");
-          const item = await crypto.certStorage.importCert("pem", pem, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
+          const item = await provider.certStorage.importCert("pem", pem, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
           assert.equal(item.type, "x509");
           assert.equal(item.issuerName, "C=BE, O=GlobalSign nv-sa, OU=For Demo Use Only, CN=GlobalSign Demo Issuing CA");
           assert.equal(item.subjectName, "C=US, ST=New Hampshire, L=Portsmouth, O=GMO GlobalSign\\, Inc., OU=Test Profile 2, CN=aegadmin");
           assert.equal(item.serialNumber, "59b32a13fb900d9b09a8986e");
           assert.equal(!!item.publicKey, true);
 
-          const pem2 = await crypto.certStorage.exportCert("pem", item);
+          const pem2 = await provider.certStorage.exportCert("pem", item);
           assert.equal(pem2, pem);
         });
 
         it("x509", async () => {
-          const item = await crypto.certStorage.importCert("x509", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]);
+          const item = await provider.certStorage.importCert("x509", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]);
           assert.equal(item.type, "x509");
 
-          const raw = await crypto.certStorage.exportCert("raw", item);
+          const raw = await provider.certStorage.exportCert("raw", item);
           assert.equal(Convert.ToHex(raw), Convert.ToHex(CERT_RAW));
         });
 
         it("throw error if imported item doesn't match to `x509` format", async () => {
-          await assert.rejects(crypto.certStorage.importCert("x509", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]));
+          await assert.rejects(provider.certStorage.importCert("x509", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]));
         });
 
       });
       context("request", () => {
         it("raw", async () => {
-          const item = await crypto.certStorage.importCert("raw", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
+          const item = await provider.certStorage.importCert("raw", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
           assert.equal(item.type, "request");
 
-          const raw = await crypto.certStorage.exportCert("raw", item);
+          const raw = await provider.certStorage.exportCert("raw", item);
           assert.equal(Convert.ToHex(raw), Convert.ToHex(REQ_RAW));
         });
 
         it("pem", async () => {
           const pem = PemConverter.fromBufferSource(REQ_RAW, "certificate request");
-          const item = await crypto.certStorage.importCert("pem", pem, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
+          const item = await provider.certStorage.importCert("pem", pem, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
           assert.equal(item.type, "request");
           assert.equal(item.subjectName, "CN=mac-mini.aegdomain1.com");
           assert.equal(!!item.publicKey, true);
 
-          const pem2 = await crypto.certStorage.exportCert("pem", item);
+          const pem2 = await provider.certStorage.exportCert("pem", item);
           assert.equal(pem2, pem);
         });
 
         it("request", async () => {
-          const item = await crypto.certStorage.importCert("request", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
+          const item = await provider.certStorage.importCert("request", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
           assert.equal(item.type, "request");
 
-          const raw = await crypto.certStorage.exportCert("raw", item);
+          const raw = await provider.certStorage.exportCert("raw", item);
           assert.equal(Convert.ToHex(raw), Convert.ToHex(REQ_RAW));
         });
 
         it("throw error if imported item doesn't match to `request` format", async () => {
-          await assert.rejects(crypto.certStorage.importCert("request", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]));
+          await assert.rejects(provider.certStorage.importCert("request", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]));
         });
       });
     });
 
     it("set/get", async () => {
-      let indexes = await crypto.certStorage.keys();
+      let indexes = await provider.certStorage.keys();
       const beforeAmount = indexes.length;
 
-      const cert = await crypto.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
-      const index = await crypto.certStorage.setItem(cert);
-      indexes = await crypto.certStorage.keys();
+      const cert = await provider.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
+      const index = await provider.certStorage.setItem(cert);
+      indexes = await provider.certStorage.keys();
       assert.equal(indexes.length - 1, beforeAmount);
 
-      const cert2 = await crypto.certStorage.getItem(index) as CryptoX509Certificate;
+      const cert2 = await provider.certStorage.getItem(index) as CryptoX509Certificate;
       assert.equal(cert2.type, "x509");
       assert.equal(cert2.serialNumber, cert.serialNumber);
     });
 
     it("removeItem", async () => {
-      let indexes = await crypto.certStorage.keys();
+      let indexes = await provider.certStorage.keys();
       const beforeAmount = indexes.length;
 
-      const cert = await crypto.certStorage.importCert("raw", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
-      const index = await crypto.certStorage.setItem(cert);
-      indexes = await crypto.certStorage.keys();
+      const cert = await provider.certStorage.importCert("raw", REQ_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509CertificateRequest;
+      const index = await provider.certStorage.setItem(cert);
+      indexes = await provider.certStorage.keys();
       assert.equal(indexes.length - 1, beforeAmount);
 
-      await crypto.certStorage.removeItem(index);
-      indexes = await crypto.certStorage.keys();
+      await provider.certStorage.removeItem(index);
+      indexes = await provider.certStorage.keys();
       assert.equal(indexes.length, beforeAmount);
     });
 
     it("getCRL", async () => {
-      const crl = await crypto.certStorage.getCRL("http://crl.comodoca.com/COMODOCertificationAuthority.crl");
+      const crl = await provider.certStorage.getCRL("http://crl.comodoca.com/COMODOCertificationAuthority.crl");
       assert.equal(crl.byteLength > 0, true);
     });
 
@@ -185,13 +186,13 @@ context("WebCrypto Socket", () => {
       });
 
       it("default", async () => {
-        const ocspResponseRaw = await crypto.certStorage.getOCSP(url, ocspRequestRaw);
+        const ocspResponseRaw = await provider.certStorage.getOCSP(url, ocspRequestRaw);
         const ocspResponse = AsnConvert.parse(ocspResponseRaw, OCSPResponse);
         assert.equal(ocspResponse.responseStatus, OCSPResponseStatus.successful);
       });
 
       it("GET", async () => {
-        const ocspResponseRaw = await crypto.certStorage.getOCSP(url, ocspRequestRaw, {
+        const ocspResponseRaw = await provider.certStorage.getOCSP(url, ocspRequestRaw, {
           method: "get",
         });
         const ocspResponse = AsnConvert.parse(ocspResponseRaw, OCSPResponse);
@@ -199,7 +200,7 @@ context("WebCrypto Socket", () => {
       });
 
       it("POST", async () => {
-        const ocspResponseRaw = await crypto.certStorage.getOCSP(url, ocspRequestRaw, {
+        const ocspResponseRaw = await provider.certStorage.getOCSP(url, ocspRequestRaw, {
           method: "post",
         });
         const ocspResponse = AsnConvert.parse(ocspResponseRaw, OCSPResponse);
@@ -207,24 +208,94 @@ context("WebCrypto Socket", () => {
       });
     });
 
-    it("getChain", async () => {
-      const index = await addCertificate(CERT_RAW);
-      await addCertificate(CERT_CA_RAW);
-      await addCertificate(CERT_ROOT_RAW);
-      const cert = await crypto.certStorage.getItem(index);
+    context("getChain", () => {
+      it("vector 1", async () => {
+        const index = await addCertificate(CERT_RAW);
+        await addCertificate(CERT_CA_RAW);
+        await addCertificate(CERT_ROOT_RAW);
+        const cert = await provider.certStorage.getItem(index);
 
-      const chain = await crypto.certStorage.getChain(cert);
-      assert.equal(chain.length, 3);
+        const chain = await provider.certStorage.getChain(cert);
+        assert.equal(chain.length, 3);
+      });
+
+      interface ChainItemParams {
+        subject: string;
+        algorithm: Algorithm;
+      }
+      type ChainParams = ChainItemParams[];
+
+      async function createCert(params: ChainItemParams): Promise<x509.X509Certificate>;
+      async function createCert(params: ChainItemParams, issuer: x509.X509Certificate, algorithm: Algorithm): Promise<x509.X509Certificate>;
+      async function createCert(params: ChainItemParams, issuer?: x509.X509Certificate, algorithm?: Algorithm): Promise<x509.X509Certificate>;
+      async function createCert(params: ChainItemParams, issuer?: x509.X509Certificate, algorithm?: Algorithm): Promise<x509.X509Certificate> {
+        const keys = await crypto.subtle.generateKey(params.algorithm, false, ["sign", "verify"]) as CryptoKeyPair;
+        if (issuer && algorithm) {
+          const cert = await x509.X509CertificateGenerator.create({
+            subject: params.subject,
+            issuer: issuer.subject,
+            publicKey: keys.publicKey,
+            signingKey: issuer.privateKey!,
+            signingAlgorithm: algorithm,
+          });
+          cert.privateKey = keys.privateKey;
+          return cert;
+        }
+        const cert = await x509.X509CertificateGenerator.createSelfSigned({
+          name: params.subject,
+          keys,
+          signingAlgorithm: params.algorithm,
+        });
+        cert.privateKey = keys.privateKey;
+        return cert;
+      }
+      /**
+       * Creates a chain of X.509 certificates based on the given parameters.
+       *
+       * @param params - An array of ChainParams representing the parameters for each certificate in the chain.
+       * @returns A Promise that resolves to an array of X.509 certificates representing the created chain.
+       */
+      async function createChain(params: ChainParams): Promise<x509.X509Certificate[]> {
+        const certs: x509.X509Certificate[] = [];
+        let issuer: x509.X509Certificate | undefined;
+        let lastAlgorithm: Algorithm | undefined;
+        for (const param of params) {
+          issuer = await createCert(param, issuer, lastAlgorithm);
+          lastAlgorithm = param.algorithm;
+          certs.push(issuer);
+        }
+        return certs;
+      }
+      it("vector 2", async () => {
+        const chainParams: ChainParams = [
+          { subject: "CN=Root", algorithm: ({ name: "RSASSA-PKCS1-v1_5", hash: "SHA-256", publicExponent: new Uint8Array([0x01, 0x00, 0x01]), modulusLength: 2048 } as Algorithm) },
+          { subject: "CN=CA #1", algorithm: ({ name: "ECDSA", namedCurve: "P-256", hash: "SHA-256" } as Algorithm) },
+          { subject: "CN=CA #2", algorithm: ({ name: "RSA-PSS", publicExponent: new Uint8Array([0x01, 0x00, 0x01]), modulusLength: 2048, hash: "SHA-256", saltLength: 32 } as Algorithm) },
+          { subject: "CN=End", algorithm: ({ name: "ECDSA", namedCurve: "P-384", hash: "SHA-384" } as Algorithm) },
+        ];
+        const chain = await createChain(chainParams);
+        let lastCert: CryptoCertificate | null = null;
+        for (let i = 0; i < chain.length; i++) {
+          const cert = chain[i];
+          const provCert = await provider.certStorage.importCert("x509", cert.rawData, chainParams[i].algorithm, ["verify"]) as CryptoX509Certificate;
+          const index = await provider.certStorage.setItem(provCert);
+          lastCert = await provider.certStorage.getItem(index);
+        }
+        assert.ok(lastCert);
+
+        const chain2 = await provider.certStorage.getChain(lastCert);
+        assert.equal(chain2.length, chain.length);
+      });
     });
 
     it("certificate public key linking", async () => {
-      const item = await crypto.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
-      const certIndex = await crypto.certStorage.setItem(item);
-      const keyIndex = await crypto.keyStorage.setItem(item.publicKey);
+      const item = await provider.certStorage.importCert("raw", CERT_RAW, { name: "RSASSA-PKCS1-v1_5", hash: "SHA-256" } as RsaHashedImportParams, ["verify"]) as CryptoX509Certificate;
+      const certIndex = await provider.certStorage.setItem(item);
+      const keyIndex = await provider.keyStorage.setItem(item.publicKey);
 
-      const cert = await crypto.certStorage.getItem(certIndex) as CryptoX509Certificate;
-      const publicKeyIndex = await crypto.keyStorage.indexOf(cert.publicKey);
-      assert.equal(publicKeyIndex, keyIndex, "Public key of the certificate is not from token");
+      const cert = await provider.certStorage.getItem(certIndex) as CryptoX509Certificate;
+      const publicKeyIndex = await provider.keyStorage.indexOf(cert.publicKey);
+      assert.equal(publicKeyIndex, keyIndex, "Public key of the certificate is not from the key storage");
     });
 
   });
